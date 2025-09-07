ddf317a091f5e535650e02e205967c0db5f38893
iambiruk <brookasefa0@gmail.com>
2025-09-07 10:27:08 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
-from . import views
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import LastView, DetailView
+from .models import Book, Library
 
-app_name = 'relationship_app'
+def list_books(request):
+    books = Book.objects.all().select_related('author')
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-urlpattern = [
-    path('books/', views.list_books, name = 'list_books'),
-    path('library/<init:pk/', views.LibraryDetailView.as_view(), name = 'library_detail'),
-]
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_queryset(sel):
+        return Library.objects.prefetch_related('books__author')
+    
