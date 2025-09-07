a674755aa9e0551788d1cafe304be93ed155d9bf
iambiruk <brookasefa0@gmail.com>
2025-09-07 10:29:16 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,49 +1,17 @@
-from django.shortcuts import render, redirect
-from django.views.generic import DetailView
-from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import  DetailView
 from .models import Book, Library
-
+from .models import Library
 
 def list_books(request):
     books = Book.objects.all().select_related('author')
     return render(request, 'relationship_app/list_books.html', {'books': books})
-
 
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
-    def get_queryset(self):
+    def get_queryset(sel):
         return Library.objects.prefetch_related('books__author')
-
-
-def custom_login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('relationship_app:list_books')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'relationship_app/login.html', {'form': form})
-
-
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('relationship_app:list_books')
-    else:
-        form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})
-
-def custom_logout(request):
-    logout(request)
-    return render(request, 'relationship_app/logout.html')
+    
