b6fe06f6e0b04b5641086a929dd0844e4435b8fb
iambiruk <brookasefa0@gmail.com>
2025-09-10 11:56:25 +0300

    Delete relationship_app directory

--- relationship_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'relationship_app'
-
-urlpatterns = [
-    path('books/', views.list_all_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-]
