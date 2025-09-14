41f9eaf7c017cf7cf0118af9ee2027de65b8f757
iambiruk <brookasefa0@gmail.com>
2025-09-14 21:51:56 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
-from django.views.generic.detail import DetailView
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 from .models import Book, Library
-from .models import Library
-
+from django.contrib.auth.forms import UserCreationForm
 
 # Function-based view to list all books
 def list_books(request):
@@ -11,19 +14,64 @@ def list_books(request):
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view to display library details
-class LibraryDetailView(DetailView):
+class LibraryDetailView(LoginRequiredMixin, DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+    login_url = '/relationship/login/'
 
     def get_queryset(self):
         return Library.objects.prefetch_related('books__author')
 
 # Class-based view to list all libraries
-class LibraryListView(ListView):
+class LibraryListView(LoginRequiredMixin, ListView):
     model = Library
     template_name = 'relationship_app/library_list.html'
     context_object_name = 'libraries'
+    login_url = '/relationship/login/'
 
+# Authentication Views
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Welcome back, {username}!')
+                next_url = request.GET.get('next', '/relationship/')
+                return redirect(next_url)
+        else:
+            messages.error(request, 'Invalid username or password.')
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'relationship_app/login.html', {'form': form})
 
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registration successful! Welcome to our library system.')
+            return redirect('relationship_app:list_books')
+        else:
+            messages.error(request, 'Please correct the errors below.')
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'relationship_app/register.html', {'form': form})
 
+@login_required(login_url='/relationship/login/')
+def logout_view(request):
+    logout(request)
+    messages.info(request, 'You have been logged out successfully.')
+    return render(request, 'relationship_app/logout.html')
+
+# Protected home view
+@login_required(login_url='/relationship/login/')
+def home_view(request):
+    return render(request, 'relationship_app/home.html')
