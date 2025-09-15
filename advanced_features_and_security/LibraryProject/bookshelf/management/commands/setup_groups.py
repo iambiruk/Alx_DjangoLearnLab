42cd017e0628093de31f60f9f194d9009e2e1283
iambiruk <brookasefa0@gmail.com>
2025-09-15 22:04:30 +0300

    Add files via upload

---
+++ advanced_features_and_security/LibraryProject/bookshelf/management/commands/setup_groups.py
@@ -0,0 +1,43 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from bookshelf.models import Book, Author
+
+class Command(BaseCommand):
+    help = 'Setup default groups and permissions'
+
+    def handle(self, *args, **options):
+        
+        book_content_type = ContentType.objects.get_for_model(Book)
+        author_content_type = ContentType.objects.get_for_model(Author)
+
+        
+        book_permissions = Permission.objects.filter(content_type=book_content_type)
+        author_permissions = Permission.objects.filter(content_type=author_content_type)
+
+        
+        viewers_group, created = Group.objects.get_or_create(name='Viewers')
+        viewers_group.permissions.set([
+            Permission.objects.get(codename='can_view_book'),
+            Permission.objects.get(codename='can_view_author'),
+        ])
+        self.stdout.write(self.style.SUCCESS('Viewers group created/updated'))
+
+        
+        editors_group, created = Group.objects.get_or_create(name='Editors')
+        editors_group.permissions.set([
+            Permission.objects.get(codename='can_view_book'),
+            Permission.objects.get(codename='can_create_book'),
+            Permission.objects.get(codename='can_edit_book'),
+            Permission.objects.get(codename='can_view_author'),
+            Permission.objects.get(codename='can_create_author'),
+            Permission.objects.get(codename='can_edit_author'),
+        ])
+        self.stdout.write(self.style.SUCCESS('Editors group created/updated'))
+
+        
+        admins_group, created = Group.objects.get_or_create(name='Admins')
+        admins_group.permissions.set(list(book_permissions) + list(author_permissions))
+        self.stdout.write(self.style.SUCCESS('Admins group created/updated'))
+
+        self.stdout.write(self.style.SUCCESS('All groups and permissions set up successfully!'))
