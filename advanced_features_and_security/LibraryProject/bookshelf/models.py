ffc8e5f980fb258ba2682e6b5948e565e13450a7
iambiruk <brookasefa0@gmail.com>
2025-09-15 21:57:03 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as 
+from django.conf import settings
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 
 class CustomUserManager(BaseUserManager):
     """Custom user manager for handling user creation with email as username."""
@@ -36,11 +39,11 @@ class CustomUserManager(BaseUserManager):
         return self._create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
-    # Remove username field and use email instead
+    
     username = None
     email = models.EmailField(_('email address'), unique=True)
     
-    # Additional required fields
+
     date_of_birth = models.DateField(_('date of birth'), null=True, blank=True)
     profile_photo = models.ImageField(
         _('profile photo'), 
@@ -60,3 +63,51 @@ class CustomUser(AbstractUser):
     class Meta:
         verbose_name = _('user')
         verbose_name_plural = _('users')
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+    description = models.TextField(blank=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='books_created'
+    )
+    updated_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='books_updated'
+    )
+
+    class Meta:
+        permissions = [
+            ("can_view_book", "Can view book details"),
+            ("can_create_book", "Can create new books"),
+            ("can_edit_book", "Can edit existing books"),
+            ("can_delete_book", "Can delete books"),
+        ]
+
+    def __str__(self):
+        return self.title
+
+ 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    class Meta:
+        permissions = [
+            ("can_view_author", "Can view author details"),
+            ("can_create_author", "Can create new authors"),
+            ("can_edit_author", "Can edit existing authors"),
+            ("can_delete_author", "Can delete authors"),
+        ]
+
+    def __str__(self):
+        return self.name
+
