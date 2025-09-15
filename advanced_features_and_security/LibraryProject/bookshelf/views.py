50c66f8af003b209870b0707c6abc41eeaae907e
iambiruk <brookasefa0@gmail.com>
2025-09-15 22:07:20 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,80 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib import messages
+from django.http import HttpResponseForbidden
+from .models import Book, Author
+from .forms import BookForm, AuthorForm
 
-# Create your views here.
+@login_required
+@permission_required('bookshelf.can_view_book', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+@login_required
+@permission_required('bookshelf.can_view_book', raise_exception=True)
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'bookshelf/book_detail.html', {'book': book})
+
+@login_required
+@permission_required('bookshelf.can_create_book', raise_exception=True)
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            book = form.save(commit=False)
+            book.created_by = request.user
+            book.save()
+            messages.success(request, 'Book created successfully!')
+            return redirect('book_detail', pk=book.pk)
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/book_form.html', {'form': form, 'action': 'Create'})
+
+@login_required
+@permission_required('bookshelf.can_edit_book', raise_exception=True)
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            book = form.save(commit=False)
+            book.updated_by = request.user
+            book.save()
+            messages.success(request, 'Book updated successfully!')
+            return redirect('book_detail', pk=book.pk)
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'bookshelf/book_form.html', {'form': form, 'action': 'Edit'})
+
+@login_required
+@permission_required('bookshelf.can_delete_book', raise_exception=True)
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        messages.success(request, 'Book deleted successfully!')
+        return redirect('book_list')
+    return render(request, 'bookshelf/book_confirm_delete.html', {'book': book})
+
+
+@login_required
+@permission_required('bookshelf.can_view_author', raise_exception=True)
+def author_list(request):
+    authors = Author.objects.all()
+    return render(request, 'bookshelf/author_list.html', {'authors': authors})
+
+@login_required
+@permission_required('bookshelf.can_create_author', raise_exception=True)
+def author_create(request):
+    if request.method == 'POST':
+        form = AuthorForm(request.POST)
+        if form.is_valid():
+            author = form.save(commit=False)
+            author.save()
+            messages.success(request, 'Author created successfully!')
+            return redirect('author_list')
+    else:
+        form = AuthorForm()
+    return render(request, 'bookshelf/author_form.html', {'form': form, 'action': 'Create'})
