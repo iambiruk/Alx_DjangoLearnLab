fc9bb3f234f77df071adb5318f683d580192186d
iambiruk <brookasefa0@gmail.com>
2025-09-21 15:32:36 +0300

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,12 +1,20 @@
 from rest_framework import generics
-from rest_frameworkk import viewsets
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny 
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
-  queryset = Book.objects.all()
-  serializer_class = BookSerializer
-
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [AllowAny]  
 class BookViewSet(viewsets.ModelViewSet):
-  queryset = Book.objects.all()
-  serializer_class = BookSerializer
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
