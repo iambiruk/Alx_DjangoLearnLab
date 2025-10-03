f44cca6e4212f8f9b594d9bdca206f799d977159
iambiruk <brookasefa0@gmail.com>
2025-10-03 16:36:47 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookListView(generics.ListAPIVIEW):
+    """
+    ListView: Retrives all books.
+    Accessible by anyone (Read-Only).
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+class BookDetailView(generics.RetrieveAPIView):
+    """
+    DetailView: Retrives a single book by ID.
+    Accessible by anyone (Read-Only)
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+class BookCreateView(generics.CreteAPIView):
+    """
+    CreateView: Allows authenticated users to add a new book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        """
+        Hook: Add extra logic when creating a book.
+        Example: You could attach the request user as metadata.
+        """
+        serializer.save()
+
+class BookUpdateView(generics.UpdateAPIView):
+    """
+    UpdateView: Allows authenticated users to modify a book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes =[permissions.IsAuthenticated]
+
+    def perform_update(self, serializer):
+        """
+        Hook: Add custom behaviour on update.
+        Example: Logging updates, enforcing business rules, etc,
+        """
+        serializer.save()
+
+class BookDeleteView(generics.DestroyAPIView):
+    """
+    DeleteView: Allows authenticated users to remove a book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
