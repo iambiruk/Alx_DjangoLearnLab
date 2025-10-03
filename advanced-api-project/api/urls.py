95ce25dac4dc206c00e95a6897fb190df12b8e35
iambiruk <brookasefa0@gmail.com>
2025-10-03 16:39:14 +0300

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name = 'book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name = 'book-list'),
     path('books/create/', BookCreateView.as_view(), name = 'book-detail'),
-    path('books/<int:pk>update/', BookUpdateView.as_view(), name = 'book-update'),
-    path('books/<int:pk>/delete', BookDeleteView.as_view(), name = 'book-dlete'),
+    path('books/update', BookUpdateView.as_view(), name = 'book-update'),
+    path('books/delete', BookDeleteView.as_view(), name = 'book-dlete'),
 ]
+
