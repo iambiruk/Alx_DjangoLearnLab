4e5c9c5d3587b3761dd0ec3084fd27312b67378c
iambiruk <brookasefa0@gmail.com>
2025-10-11 20:39:39 +0300

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,8 +76,9 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'blog_db',
+       'USER': 'postgres', 
     }
 }
 
@@ -124,3 +125,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog','static')]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
