f67d54ee1414515078da2abb02db595a324bb38a
iambiruk <brookasefa0@gmail.com>
2025-10-11 20:40:43 +0300

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -79,6 +79,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
        'NAME': 'blog_db',
        'USER': 'postgres', 
+        'PASSWORD': 'your_password',  # your password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -125,4 +128,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog','static')]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
